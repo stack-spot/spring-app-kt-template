d98edeb20d0513f5853d53e63bd24392dd321bbe
StackSpot Admin <admin@stackspot.com>
2022-03-18 14:54:36 +0000

    Update public repository

--- templates/infra/src/main/kotlin/group_id_folder/{{computed_inputs.camel_project_name}}CdkApp.kt
+++ templates/infra/src/main/kotlin/group_id_folder/{{computed_inputs.camel_project_name}}CdkApp.kt
@@ -11,11 +11,10 @@ const val APP_PORT = 8080
 
 fun main() {
     val app = App()
-    lateinit var stage: Any
-    try {
-        stage = app.node.tryGetContext("stage")
+    val stage = try {
+        app.node.tryGetContext("stage")
     } catch (ex: Exception) {
-        throw Error("Stage parameter is required")
+        throw Error("Stage not found in CDK context, either use stk deploy <stage> or the cdk -c stage=<stage> option")
     }
     val stageObject =
         jacksonObjectMapper().readValue(Paths.get("..", "stages", "$stage.json").toFile(), Stage::class.java)
